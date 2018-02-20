c8141325db1cb9c43fab5bc0b07841daf791a5de
Yuhan Lin <linyuhan0315@hotmail.com>
2018-02-20 19:42:28 +0000

    Deleted old collections and renamed csv files so the filenames are short enough for windows

--- scripts/yuhan.py
+++ scripts/yuhan.py
@@ -167,7 +167,7 @@ def onPeriodic():
                         if (training_data['file'] != None):
                                 training_data['file'].close()
 
-                        training_data['file'] = open('EMG_Training_Data/emg_training_data_'+label+'_'+datetime.datetime.now().strftime("%Y-%m-%d@%H:%M:%S")+'.csv','w+')
+                        training_data['file'] = open(label+'_'+datetime.datetime.now().strftime("%Y-%m-%d@%H:%M:%S")+'.csv','w+')
                         training_data['csv_writer'] = csv.writer(training_data['file'])
 
                 elif (mode == 'predicting'):
